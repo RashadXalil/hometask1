604955e1e11bced778fa3480ff4fd60c8edb3e66
RashadXalil <rashadmkh@code.edu.az>
2021-08-17 09:24:09 +0400

    task1

---
+++ cp4/cp4/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace cp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            calc();
+        }
+        static void calc()
+
+        {
+            Console.WriteLine("Ilk ededi daxil et : ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("ikinci  ededi daxil et : ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("emeliyyati secin + ; - : * ; /");
+            string emeliyyat = Console.ReadLine();
+
+            if (emeliyyat == "+")
+            {
+                Console.WriteLine(num1+num2);
+            }
+            else if (emeliyyat == "-")
+            {
+                Console.WriteLine(num1-num2);
+            }
+            else if (emeliyyat == "*")
+            {
+                Console.WriteLine(num1*num2);
+            }
+            else if (emeliyyat == "/")
+            {
+                Console.WriteLine(num1/num2);
+            }
+            else
+            {
+                Console.WriteLine("Duzgun emeliyyat daxil edin ");
+            }
+
+
+
+        }
+    }
+}
